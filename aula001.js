b60ecb7ff4bc2ce658bef3e5745d43ea0a55ceb0
frederico <fredericoufsj1gmail.com>
2021-07-01 19:37:54 -0300

    first commit

---
+++ aula001.js
@@ -0,0 +1,12 @@
+var numero = 9;
+for (var contador = 1; contador <= 9; contador++) { console.log(numero * contador); }
+var resultado = [];
+var frase = ["Xau brigado"];
+console.log(frase);
+
+var resultado = [];
+var frase = "Xau Brigado!";
+for (var contador = 0; contador < frase.length; contador++) {
+    resultado.push(frase[contador]);
+};
+console.log(resultado)
